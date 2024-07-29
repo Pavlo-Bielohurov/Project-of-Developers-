90a3a698c7867ee5b56d3662f4cb65703bbf5e79
Makanin Aleksey <115780875+FizrukAlekesey@users.noreply.github.com>
2024-07-29 22:12:43 +0300

    start

--- src/JS/work-together.js
+++ src/JS/work-together.js
@@ -1,74 +1,70 @@
 
-/*const modalTrigger = document.getElementsByClassName("trigger")[0];
+import axios from 'axios';
 
-const windowInnerWidth = document.documentElement.clientWidth;
-const scrollbarWidth = parseInt(window.innerWidth) - parseInt(document.documentElement.clientWidth);
-
-const bodyElementHTML = document.getElementsByTagName("body")[0];
-
-const modalBackground = document.getElementsByClassName("foot-modal")[0];
-
-const modalClose = document.getElementsByClassName("modal-close-btn")[0];
-const modalActive = document.getElementsByClassName("modal-open")[0];
-
-function bodyMargin() {
-    bodyElementHTML.style.marginRight = "-" + scrollbarWidth + "px";
-}
-bodyMargin();
-
-modalTrigger.addEventListener("click", function () {
-    // делаем модальное окно видимым
-    modalBackground.style.display = "block";
-    if (windowInnerWidth >= 1366) {
-        bodyMargin();
-    }
-    modalActive.style.left = "calc(50% - " + (175 - scrollbarWidth / 2) + "px)";
-});
-
-modalClose.addEventListener("click", function () {
-    modalBackground.style.display = "none";
-    if (windowInnerWidth >= 1366) {
-        bodyMargin();
-    }
-});*/
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var modalButtons = document.querySelectorAll('.modal-open'),
-        closeButtons = document.querySelector('.modal-close-btn');
-    
-    
-    modalButtons.forEach(function(item){
-       
-       item.addEventListener('click', function(e) {
-          
-          e.preventDefault();
-          var modalId = this.getAttribute('data-modal'),
-              modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-          
-          modalElem.classList.add('active');
-          overlay.classList.add('active');
-       }); // end click
-    }); // end foreach
- }); // end ready
-
- closeButtons.forEach(function(item){
-    item.addEventListener('click', function(e) {
-       var parentModal = this.closest('.modal');
-       parentModal.classList.remove('active');
-       overlay.classList.remove('active');
-    });
- });
-
-
-
-
- import axios from 'axios';
-
-const URL = 'https:';
+const URL = 'https://portfolio-js.b.goit.study/api';
 axios.defaults.baseURL = URL;
 
 export async function makePost(user) {
   const { data, status, statusText } = await axios.post('/requests', user);
   return { data, status, statusText };
 }
+
+
+const refs = {
+    form: document.querySelector('.js-footer-form'),
+    spinner: document.querySelector('.js-loader-footer'),
+    btnFooter: document.querySelector('.js-btn-footer'),
+  };
+
+function hideSpinner() {
+  refs.spinner.classList.add('is-hidden');
+  refs.btnFooter.classList.remove('is-hidden');
+}
+
+function showSpinner() {
+  refs.spinner.classList.remove('is-hidden');
+  refs.btnFooter.classList.add('is-hidden');
+}
+
+export { hideSpinner, showSpinner };
+
+import {
+  addInlineInfoNotifyStyle,
+  emptyNofify,
+  errorNotify,
+  infoNotify,
+} from './foot.bac.styls';
+
+export function handlerPost(evt) {
+  evt.preventDefault();
+
+  const { emailContact, comment } = evt.target.elements;
+  const emailValue = emailContact.value.trim();
+  const commentValue = comment.value.trim();
+
+  if (!commentValue) {
+    emptyNofify();
+    return;
+  }
+
+  showSpinner();
+
+  makePost({ email: emailValue, comment: commentValue })
+    .then(() => {
+      infoNotify();
+      addInlineInfoNotifyStyle();
+
+      evt.target.reset();
+    })
+    .catch(() => {
+      errorNotify();
+      return;
+    })
+    .finally(() => {
+      hideSpinner();
+    });
+}
+
+refs.form.addEventListener('submit', handlerPost);
+
+refs.spinner.classList.add('is-hidden');
