1b328710c595a6fe3eab704313a1109d6c677b91
Pavlo-Bielohurov <158511567+Pavlo-Bielohurov@users.noreply.github.com>
2024-07-30 18:52:42 +0300

    1

--- src/JS/about-me-swiper.js
+++ src/JS/about-me-swiper.js
@@ -1,35 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const slides = document.querySelectorAll('.swiper-slide-skills');
-  const nextButton = document.querySelector('.swiper-button-next');
-  const swiperWrapper = document.querySelector('.swiper-wrapper-skills');
-  let currentIndex = 0;
+const swiper = new Swiper('.about-me-swiper', {
+  modules: [Navigation, Keyboard],
+  navigation: {
+    nextEl: '.swiper-button-wrap .aboutme-swiper-btn',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+  centeredSlidesBounds: true,
+  speed: 400,
+});
 
-  function updateSwiper() {
-    const width = window.innerWidth;
-    let visibleSlides;
-
-    if (width <= 375) {
-      visibleSlides = 2;
-    } else if (width <= 768) {
-      visibleSlides = 3;
-    } else if (width >= 1440) {
-      visibleSlides = slides.length;
-    } else {
-      visibleSlides = slides.length;
-    }
-
-    const slideWidth = slides[0].clientWidth + 20; // slide width + margin
-    const offset = -currentIndex * slideWidth;
-    swiperWrapper.style.transform = translateX(${offset}px);
+const nextButton = document.querySelector(
+  '.swiper-button-wrap .aboutme-swiper-btn'
+);
+nextButton.addEventListener('click', () => {
+  if (window.innerWidth >= 1440) {
+    swiper.slides[swiper.activeIndex].parentNode.appendChild(
+      swiper.slides[swiper.activeIndex]
+    );
+    swiper.update();
   }
-
-  nextButton.addEventListener('click', () => {
-    slides[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % slides.length;
-    slides[currentIndex].classList.add('active');
-    updateSwiper();
-  });
-
-  window.addEventListener('resize', updateSwiper);
-  updateSwiper();
 });
