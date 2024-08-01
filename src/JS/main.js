70d3ac2f7802f1d91a64fb0c3d5516da05f36cb4
Serhii Tyron <mestn1y@hotmail.com>
2024-08-01 22:45:28 +0300

    project-done

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,1 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > window.innerHeight / 2) {
+      scrollToTopBtn.style.display = 'block';
+    } else {
+      scrollToTopBtn.style.display = 'none';
+    }
+  });
+
+  scrollToTopBtn.addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+});
